7173aa7bddfde41df08ac3ee39388d2fe8bed287
Aniruddh Upadhyay <aniruddh622003@gmail.com>
2021-11-04 01:42:04 +0530

    Merge Sort

--- src/SortingVisualiser/SortingVisualiser.js
+++ src/SortingVisualiser/SortingVisualiser.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import { getMergeSortAnimations } from "./algorithms/SortingAlgorithms";
+import { ANIMATION_SPEED_MS, PRIMARY_COLOR, SECONDARY_COLOR } from "./settings";
 import "./SortingVisualiser.css";
 
 function SortingVisualiser() {
@@ -7,6 +9,30 @@ function SortingVisualiser() {
   useEffect(() => {
     resetArray();
   }, []);
+
+  function mergeSort() {
+    const animations = getMergeSortAnimations(array);
+    for (let i = 0; i < animations.length; i++) {
+      const arrayBars = document.getElementsByClassName("array-bar");
+      const isColorChange = i % 3 !== 2;
+      if (isColorChange) {
+        const [barOneIdx, barTwoIdx] = animations[i];
+        const barOneStyle = arrayBars[barOneIdx].style;
+        const barTwoStyle = arrayBars[barTwoIdx].style;
+        const color = i % 3 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
+        setTimeout(() => {
+          barOneStyle.backgroundColor = color;
+          barTwoStyle.backgroundColor = color;
+        }, i * ANIMATION_SPEED_MS);
+      } else {
+        setTimeout(() => {
+          const [barOneIdx, newHeight] = animations[i];
+          const barOneStyle = arrayBars[barOneIdx].style;
+          barOneStyle.height = `${newHeight}px`;
+        }, i * ANIMATION_SPEED_MS);
+      }
+    }
+  }
 
   function randomIntFromInterval(min, max) {
     return Math.floor(Math.random() * (max - min - 1) + min);
@@ -19,16 +45,23 @@ function SortingVisualiser() {
     }
     setArray(arr);
   };
+
   return (
-    <div className="array-cont">
-      {array.map((value, id) => (
-        <div
-          className="array-bar"
-          key={id}
-          style={{ height: `${value}px` }}
-        ></div>
-      ))}
-    </div>
+    <>
+      <div className="array-cont">
+        {array.map((value, id) => (
+          <div
+            className="array-bar"
+            key={id}
+            style={{ height: `${value}px` }}
+          ></div>
+        ))}
+      </div>
+      <div className="buttons">
+        <button onClick={() => resetArray()}>Generate New Array</button>
+        <button onClick={() => mergeSort()}>Merge Sort</button>
+      </div>
+    </>
   );
 }
 
---
+++ src/SortingVisualiser/settings.js
@@ -0,0 +1,3 @@
+export const PRIMARY_COLOR = "#2b95ff";
+export const SECONDARY_COLOR = "#d41e1e";
+export const ANIMATION_SPEED_MS = 10;
